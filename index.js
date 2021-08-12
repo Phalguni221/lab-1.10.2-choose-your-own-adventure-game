2e3e7670b0f3331f2bf2b6109d0dcf94636ba756
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-08-12 14:21:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-let userName = window.prompt('Please enter your name:')
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+}if(secondAnswer === 'follow'){
+    let thirdAnswer = window.prompt("You come across a room full of treasures and mysteries! You lucky cat!!(Type OK to continue)")
+
+} else if(secondAnswer === "continue"){
+    let thirdAnswer = window.prompt("Go straight ahead to reach the golden door!")
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+        let thirdAnswer = window.prompt("Keep walking until you reach the Golden Door")
+    }
+
+
+
+
 let userScore = 0;
 
 for(let i = 0; i < questions.length; i++){
